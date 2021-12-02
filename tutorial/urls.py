5158a60960df62b378bc460ce43996bca081352f
Pax <13646646+paxcodes@users.noreply.github.com>
2021-12-01 17:56:07 -0800

    feat: register viewsets' urls

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,21 +1,21 @@
-"""tutorial URL Configuration
+from django.urls import include, path
+from rest_framework import routers
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+from tutorial.quickstart import views
+
+# Because we are using viewsets instead of views, we can automatically generate the
+# URL conf for our API, by simply registering the viewsets with a router class.
+# If we need more control over the API URLs we can drop down to using regular
+# class-based views, and writing the URL conf explicitly.
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", include(router.urls)),
+    # Include default login and logout views for use with the browsable API.
+    # This is optional if your API requires authentication and you want to use the
+    # browsable API.
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
